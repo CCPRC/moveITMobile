4e88bf0cc0179572c17fbc63724ce0cb8f18bf11
Imee Cuison <imee@dynepic.com>
2016-04-03 21:44:28 -0400

    bottom border in header removed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,32 @@
 angular.module('app.controllers', [])
-  
+
 .controller('classesTabDefaultPageCtrl', function($scope) {
 
+  $scope.date = new Date();
+
 })
-   
+
 .controller('membersTabDefaultPageCtrl', function($scope) {
 
 })
-   
+
 .controller('assessmentsTabDefaultPageCtrl', function($scope) {
 
 })
-      
+
 .controller('measurementsTabDefaultPageCtrl', function($scope) {
 
 })
-   
+
+.controller('classCtrl', function($scope){
+  $scope.date = new Date();
+
+})
+
 .controller('yogaClassCtrl', function($scope) {
 
 })
-   
+
 .controller('loginCtrl', function($scope) {
 
 })
- 
